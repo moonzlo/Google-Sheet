6cfa6489870f31a61a3946b3b03a921b2882d4ea
moonz <mihail.monz@gmail.com>
2019-01-23 19:12:52 +0300

    ver 1.0

---
+++ func.py
@@ -0,0 +1,205 @@
+from gspread_formatting import *
+from bs4 import BeautifulSoup
+from operator import itemgetter
+import requests, json, time
+
+
+def exchange_rate():
+    '''Получает актуальный курс рос.рубля к бел.рублю'''
+    html = get_htmls()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    tag = soup.find_all('span', class_='cc-result')
+    a = tag[1].text.strip()
+
+    b = re.findall(r'[0-9.]', '{}'.format(a))
+    num = str().join(b)
+    number = float(num)
+
+    return number
+
+
+
+def final_price(exchange_rate, rub):
+
+    price = exchange_rate*rub
+
+    procent = price / 100 * 8
+    total = int(price) + int(procent)
+    return total
+
+
+def get_htmls():
+    # url = 'https://www.sima-land.ru/3295133'
+    url = 'https://rub.ru.currencyrate.today/byn'
+
+    headers = {'accept': '*/*',
+               'user-agent':
+                   'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5'}
+    session = requests.Session()  # Иметирует сессию клиента.
+    request = session.get(url, headers=headers)
+
+    return request.content
+
+
+
+# Функция принимает на вход список из экземпляров класса Stroka
+
+def group_data(factory_list):
+    '''Цель данной функции, объеденить строки в списки, для удобства взаимодействия и расчётов'''
+    sorted_list = []  # Список кластеризованных экземпляров класса Stroka, в списки.
+
+    klaster = []
+
+    for x in factory_list:
+        if bool(x.name) != False:
+            klaster.append(x)
+        else:
+            klaster.append(x)
+            sorted_list.append(klaster)
+            klaster = []
+
+    return sorted_list
+
+
+# Запускать в самом конце.
+
+def order_amount(sorted_list, sheet):
+    '''Данная функция сумирует все цены, и выводит общую сумму заказа'''
+
+    summ = []
+    for li in sorted_list:
+        for i in li:
+            if bool(i.name) != False:
+
+                vale = str(i.unit_item).strip()
+                amount = i.item_value
+                summin = int(amount) * int(vale)
+                summ.append(summin)
+
+            else:
+                # Обновляем итог, красим ячейку в дефолтный цвет и выделяем жиром.
+                sheet.update_acell('K{}'.format(i.str_number), sum(summ))
+                default_format = CellFormat(backgroundColor=color(30, 10, 10), textFormat=textFormat(bold=True))
+                format_cell_range(sheet, 'K{}'.format(i.str_number), default_format)
+
+                # Обвноляем данные орг.сбора и конвертация в бел.рубль
+                total = final_price(exchange_rate(), sum(summ))
+
+                sheet.update_acell('L{}'.format(i.str_number), total)
+                default_format = CellFormat(backgroundColor=color(30, 10, 10), textFormat=textFormat(bold=True))
+                format_cell_range(sheet, 'L{}'.format(i.str_number), default_format)
+
+
+def removal(table_data):
+    data = table_data
+    black = []
+
+    for i in data:
+        if i.get('Дейсвтие') == 'Удаляю':
+            value = []
+            value.append(i)
+            data.remove(i)
+            for x in data:
+                if value[0].get('Фамилия и имя') == x.get('Фамилия и имя') and value[0].get('Артикул') == x.get('Артикул'):
+                    data.remove(x)
+
+
+    return data
+
+def def_color(deck, table_data):
+    '''Данная функция сулжит для предварительного окраса всех строк в белый цвет'''
+    for i in table_data:
+        num = len(table_data) + 1
+        table_numbers = 'A6:M{}'.format(num)
+        default_format = CellFormat(backgroundColor=color(1, 1,1), textFormat=textFormat(bold=False))
+        format_cell_range(deck, table_numbers, default_format)
+
+def def_table(deck, table_data):
+    '''Цель данной функции, предварительная очистка всего поля от всех данных'''
+    stop = len(table_data) + 1
+    value = 'A6:M{}'.format(stop)
+    cell_list = deck.range(value)
+
+    for cell in cell_list:
+        cell.value = ' '
+
+    deck.update_cells(cell_list)
+
+
+def initor(sheet):
+    '''сортирует все данные по алфовиту + добавляет пустые строки в конеце кластера'''
+
+    # Получаем отсортированный по именам список. Пустые поля попадают в самый верх.
+    list_of_dicts = sheet
+    # list_of_dicts.sort(key=itemgetter('Фамилия и имя'))
+    # Убираем все пустые строки, и сортируем по алфовиту.
+    new = []
+    new.extend(list_of_dicts)
+    sorted_list_new = []
+
+    for i, c in enumerate(new):
+        a = str(c.get('Фамилия и имя')).strip()
+
+        if bool(a) == True:
+            sorted_list_new.append(c)
+
+
+    sorted_list_new.sort(key=itemgetter('Фамилия и имя'))
+
+
+    def work_list(list_dict):
+        # Пустая строка разделитель.
+        zaglushka ={
+            "Отметка времени": "",
+            "Фамилия и имя": "",
+            "Действие": " ",
+            "Наименование (кратко)": " ",
+            "Артикул": " ",
+            "Ссылка на товар": " ",
+            "Цена за единицу товара": " ",
+            "Количество (нужное Вам)": " ",
+            "Количество для минимального заказа": " ",
+            "Примечание": "",
+            "Цена за доставку (если есть)": " ",
+            "Сумма заказа в РОС.РУБЛЯХ": " ",
+            "Итоговая сумма (+орг.сбор 9%)": " "
+        }
+
+        sorted_dict_list = []
+
+        for i, c in enumerate(list_dict):
+            try:
+
+                if list_dict[i].get('Фамилия и имя') != list_dict[i + 1].get('Фамилия и имя'):
+                    sorted_dict_list.append(c)
+                    a = zaglushka.copy()
+                    a.update({'Заглушка для:':list_dict[i].get('Фамилия и имя')})
+                    sorted_dict_list.append(a)
+
+                else:
+                    sorted_dict_list.append(c)
+            except IndexError:  # Когда кончается список
+                sorted_dict_list.append(c)
+                a = zaglushka.copy()
+                a.update({'Заглушка для:': list_dict[i].get('Фамилия и имя')})
+                sorted_dict_list.append(a)
+
+
+        return sorted_dict_list  # Возвращаем отсортированные список словарей + загрулшки.
+
+    # Передаем в функцию отсортированные по алфовиту, получаем записанные файл и список с загрушками.
+    sorted_list = work_list(sorted_list_new)
+
+    # Добавляем индекс строки в конце каждого словаря.
+
+    nums = 6  # Начальная строка, от которой будет идти обновление
+    for i, c in enumerate(sorted_list):
+        sorted_list[i].update({'Строка':nums})
+        nums += 1
+
+    # Записываем пронумерованные строки в файл.
+    with open('iter_sort.json', 'w') as file:
+        json.dump(sorted_list, file, indent=2, ensure_ascii=False)
+
+    return sorted_list
---
+++ main.py
@@ -0,0 +1,198 @@
+import gspread
+import time
+from oauth2client.service_account import ServiceAccountCredentials
+import pprint
+from gspread_formatting import *
+import re
+import json
+import requests
+from bs4 import BeautifulSoup
+from func import *
+
+
+def get_html(url):
+    agent = 'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5'
+    headers = {'accept': '*/*',
+               'user-agent': agent}
+    session = requests.Session()  # Иметирует сессию клиента.
+    request = session.get(url, headers=headers)
+
+    return request.content
+
+
+def get_sheet():
+    '''отдаёт список с словарями, количество словарей равно количесту заполненных строк'''
+
+    scope = ['https://www.googleapis.com/auth/drive']
+
+    creds = ServiceAccountCredentials.from_json_keyfile_name('login.json', scope)
+    client = gspread.authorize(creds)
+
+    sheet = client.open('test1').sheet1
+
+    leg = sheet.get_all_records()  # Список внутри которого словари (количество словареий равно кличеству строк)
+    mass = leg[4:]  # Пропускаем пустые строки
+
+    return sheet
+
+
+def reed_json():
+    with open('test.json', 'r', encoding='utf-8') as js:  # открываем файл на чтение
+        data = json.load(js)  # загружаем из файла данные в словарь data
+
+    return data
+
+
+class Stroka(object):
+
+    def __init__(self, dictionary, deck):
+        '''Принимает словарь из списка открытого json файла, задает базовые значения классу.'''
+
+
+        self.sheet = deck  # Объект доски.
+        self.time = dictionary.get('Отметка времени')
+        self.name = dictionary.get('Фамилия и имя')
+        self.doing = dictionary.get('Дейсвтие')
+        if self.doing == None:
+            self.doing = ''
+        self.items_name = dictionary.get('Наименование (кратко)')
+        self.article = dictionary.get('Артикул')
+        self.item_url = dictionary.get('Ссылка на товар')
+        self.unit_item = dictionary.get('Цена за единицу товара (в рос.руб)')
+        if self.unit_item == None:
+            self.unit_item = ''
+        self.item_value = dictionary.get('Количество (нужное Вам)')
+        self.min_value = dictionary.get('Количество для минимального заказа')
+        self.notice = dictionary.get('Примечание')
+        self.delivery = dictionary.get('Цена за доставку (если есть)')
+        self.str_number = dictionary.get('Строка')
+
+        # self.klaster = dictionary.get(keys[13])  # Параметр для заглушки, хранит имя кластера
+
+
+    def table_update(self):
+        '''Данный метод записывает данные непосредственно в таблицу'''
+
+        scope = ['https://www.googleapis.com/auth/drive']
+        # Авторизация и получения доступа к доске.
+        creds = ServiceAccountCredentials.from_json_keyfile_name('login.json', scope)
+        client = gspread.authorize(creds)
+        sheet = client.open('test1').sheet1
+        namers = [self.time, self.name, self.doing, self.items_name, self.article, self.item_url, self.unit_item,
+                  self.item_value, self.min_value, self.delivery, '', '', self.notice]
+
+        if bool(self.name) != False:
+
+
+            cell_list = self.sheet.range('A{}:M{}'.format(self.str_number, self.str_number))
+
+            # Обновляем каждоу строку.
+            for cell, name in zip(cell_list, namers):
+                cell.value = name
+
+            # Update in batch
+            self.sheet.update_cells(cell_list)
+
+    def product_name_update(self):
+        '''Метод получает полное название + цену товара, и обновляет их в параметрах экземпляра класса'''
+
+        base = 'https://www.sima-land.ru/{}'.format(self.article)
+        html = get_html(base)
+        soup = BeautifulSoup(html, 'html.parser')
+        table_numbers = 'A{}:M{}'.format(self.str_number, self.str_number)
+
+        # Проверям на товар партнёра
+        try:
+            tag = soup.find('div', class_='flags').find_all('span')
+
+            tag_list = []
+
+            for i in tag:
+                par = i.text.strip()
+                tag_list.append(par)
+
+            table_numbers = 'A{}:M{}'.format(self.str_number, self.str_number)
+
+            if 'Товар партнёра' in tag_list:
+                # Если тэг Товар партнёра Найден, окрашивает в берюзовый всю строку.
+                default_format = CellFormat(backgroundColor=color(250, 10, 10), textFormat=textFormat(bold=False))
+                format_cell_range(self.sheet, table_numbers, default_format)
+
+
+        except Exception as error:
+            print('Ошибка тута',error)
+
+        # Обновляем название и цену
+        try:
+            price = soup.find('span', class_='price__val').find('span').text
+
+            a = re.findall(r'\d+', '{}'.format(price))
+            num = str().join(a)
+            self.unit_item = int(num)
+
+            price_name = soup.find('div', class_='title').find('h1')
+            for i in price_name:
+                self.items_name = i
+        except AttributeError:
+            self.unit_item = 'Ошибка'
+            self.items_name = 'Ошибка артикла'
+
+        # Проверяем на наличие товара.
+        try:
+            status = soup.find('td', class_='a_m purchase__cell purchase__cell_zero').text.strip()
+            if status == 'Нет в наличии':
+                default_format = CellFormat(backgroundColor=color(1, 0, 240), textFormat=textFormat(bold=False))
+                format_cell_range(self.sheet, table_numbers, default_format)
+        except AttributeError:
+            pass
+
+
+
+
+
+# -----------------ЗАПУСК
+deck = get_sheet()
+leg = deck.get_all_records()  # Список внутри которого словари (количество словареий равно кличеству строк)
+mass = leg[4:]  # Пропускаем пустые строки
+
+deleter = removal(mass)
+
+
+# Инициализирует таблицу, записывая отсортированные данные в json iter_sort.json
+table_data = initor(deleter)  # Получем список словарей (всех строк таблицы)
+
+# Очищаем все строки.
+def_table(deck, leg)
+
+# Окрашиваем все строки в белый.
+def_color(deck,leg)
+
+def str_generator(table_data):
+    '''Данная функция служит фабрикой, оборачивая каждую строку таблицы в классы Strok'''
+    class_list = []
+
+    for i in table_data:
+        # Класс принимает два аргмента, словрь с данынми о строке, и текущую доску.
+        data = Stroka(i, deck)
+        # Обновляем все базовые значения класса.
+        if bool(data.name) != False:
+            data.product_name_update()  # Получаем и обновляем настоящие имя и цену товара.
+
+        class_list.append(data)
+
+
+    return class_list
+
+# Фабрика, генератор экземпляров строки.
+factory = str_generator(table_data)
+
+
+
+# Кластеризуем экземпляры класса, и обновляем сумму.
+klaster = group_data(factory)  # Возвращает список из кластеров (экземпляров класса). [[Вася пупкин],[Вася Петров]]
+order_amount(klaster, deck)  # Подсчитывает суммы, вносит изменения в таблицу.
+
+# Обновляем все строки.
+for st in factory:
+    st.table_update()
+    time.sleep(1)
